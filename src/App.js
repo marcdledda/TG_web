2280ea3ee23c5f711d59a43cdfee88e1479f0d16
marcdledda <marcdl636@gmail.com>
2018-06-12 01:54:20 -0500

    started nav bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
+import Nav from './components/Nav';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        TEST
+      <div className="container">
+        <Nav />
       </div>
     );
   }
 }
+
 
 export default App;
