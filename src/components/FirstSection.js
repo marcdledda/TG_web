6ab045b9a7489d4ff208497201a936569736ce54
marcdledda <marcdl636@gmail.com>
2018-06-12 16:14:30 -0500

    continued overview

--- src/components/FirstSection.js
+++ src/components/FirstSection.js
@@ -12,7 +12,7 @@ class FirstSection extends Component {
                         <h1 className="mt-2 mb-3">Testing Grounds</h1>
                         <button className="download px-4 py-2"><h4 className="m-0">Download Now</h4></button>
                     </div>
-                    <div className="col-sm embed-responsive embed-responsive-16by9">
+                    <div className="theVideo col-sm embed-responsive embed-responsive-16by9">
                         <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/IJNR2EpS0jw" frameBorder="0" allowFullScreen></iframe>
                     </div>
                 </div>
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -4,9 +4,18 @@ import './Overview.css';
 class Overview extends Component {
     render(){
         return(
-            <div className="py-lg-5 mx-auto my-lg-5 row">
+            <div className="py-lg-5 mx-auto my-lg-5 justify-content-center row overview">
                 <div className="col-12 text-center">
                     <h2>Overview</h2>
+                    <h4>Welcome to the Testing Grounds, Subject 24. Here you will be tested on two things: Your dexterity and reflex.</h4>
+                </div>
+
+                <div className="col-sm-4 mt-3 text-center">
+                    <h2>Dexterity Test</h2>
+                </div>
+
+                <div className="col-sm-4 mt-3 text-center">
+                    <h2>Reflex Test</h2>
                 </div>
             </div>
         );
