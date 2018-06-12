1f16a03a430ac544796cbc647ccbc576caec295d
marcdledda <marcdl636@gmail.com>
2018-06-12 13:41:36 -0500

    create first section with logo, download button, and trailer video

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Nav from './components/Nav';
+import FirstSection from './components/FirstSection';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <Nav />
+      <div className="container App">
+        <FirstSection />
       </div>
     );
   }
