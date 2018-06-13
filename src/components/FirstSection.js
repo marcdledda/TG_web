c76fde32478061824801ed121d944104f9a33765
marcdledda <marcdl636@gmail.com>
2018-06-13 15:26:02 -0500

    added background img for overview and added icon

--- src/components/FirstSection.js
+++ src/components/FirstSection.js
@@ -10,7 +10,7 @@ class FirstSection extends Component {
                     <div className="testingGround col-sm-5 text-center">
                         <img className="img-fluid" src={Logo} alt="Logo of Testing"/>
                         <h1 className="mt-2 mb-3">Testing Grounds</h1>
-                        <button className="download px-4 py-2"><h4 className="m-0">Download Now</h4></button>
+                        <a href={Logo} download="Testing Grounds VR"><button className="download px-4 py-2"><h4 className="m-0">Download Now</h4></button></a>
                     </div>
                     <div className="theVideo col-sm embed-responsive embed-responsive-16by9">
                         <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/IJNR2EpS0jw" frameBorder="0" allowFullScreen></iframe>
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -7,15 +7,21 @@ class Overview extends Component {
             <div className="py-lg-5 mx-auto my-lg-5 justify-content-center row overview">
                 <div className="col-12 text-center">
                     <h2>Overview</h2>
-                    <h4>Welcome to the Testing Grounds, Subject 24. Here you will be tested on two things: Your dexterity and reflex.</h4>
+                    <h4>Welcome to the Testing Grounds, Subject 24. Here you will be tested on two things: </h4>
                 </div>
 
-                <div className="col-sm-4 mt-3 text-center">
+                <div className="col-sm-4 my-4 mx-lg-4 text-center dexTest">
                     <h2>Dexterity Test</h2>
+                    <h4>Dodge incoming spheres</h4>
                 </div>
 
-                <div className="col-sm-4 mt-3 text-center">
+                <div className="col-sm-4 mt-lg-4 mb-4 mx-lg-4 text-center refTest">
                     <h2>Reflex Test</h2>
+                    <h4>Shoot the dummies</h4>
+                </div>
+
+                <div className="col-12 text-center">
+                    <h4>Failure to perform adequately will result in punishment. Thank you for participating!</h4>
                 </div>
             </div>
         );
